ca308e102c08ea781c3f13f3c3fe86e58bde65b5
Alvaro <alvaroramirez1600@gmail.com>
2021-01-29 22:01:26 +0100

    Mapa con ubicacion del evento

--- frontend/src/app/components/eventdetails/eventdetails.component.ts
+++ frontend/src/app/components/eventdetails/eventdetails.component.ts
@@ -18,17 +18,20 @@ export class EventdetailsComponent implements OnInit {
   public evento: any;
   public usuarios: any;
 
+  
+  public marcadores = L.layerGroup();
+  public mapa: any;
   public datosevento = {
     latitud: 0,
     longitud: 0,
     pointer: L.layerGroup()
   };
-  public mapa: any;
 
-  constructor(public servicioEventos: EventosService, private router: Router,private rutaActiva: ActivatedRoute, private servicioUser: UsuariosService) { }
+  constructor(public servicioEventos: EventosService, private router: Router,private rutaActiva: ActivatedRoute, private servicioUsuarios: UsuariosService) { }
 
 
   ngOnInit() {
+    this.cargarMarcadores(this.marcadores);
     this.id = this.rutaActiva.snapshot.params.id;
     localStorage.setItem('idEvento', this.id);
     console.log("id: ", this.id);
@@ -38,12 +41,21 @@ export class EventdetailsComponent implements OnInit {
         res[0].fecha = this.formatDate(res[0].fecha);
         this.evento = res;
         this.usuarios = res[0].lista_users;
-        
+        const ubi = {
+          lat: res[0].latitud,
+          long: res[0].longitud
+        }
+        this.cargarMapa(ubi);
       },
       err => {
         console.log(err);
       }
     );
+    
+    const ubi= {
+      latitud: 30,
+      longitud: 12,
+    }
   }
 
   borrarEvento() {
@@ -110,7 +122,65 @@ export class EventdetailsComponent implements OnInit {
         }
       )
     }
+  }
+
+
+  /* MAPA */
+  cargarMapa(datos) {       // CREAR LOS MAPAS
+    this.mapa = L.map('mapaevento',{
+      center: [datos.lat, datos.long],
+      zoom: 13
+  });
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attributon: '&copy;<a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(this.mapa);
+    
+    this.marcadores.addTo(this.mapa);
+
+    this.mapa.setZoom(10);
+  }
+
+  cargarMarcadores(marcas: any) {  // CARGAR LOS MARCADORES DE LOS USUARIOS
+
+    this.servicioEventos.getEventos().subscribe(
+      res => {
+        res.forEach(event => {
+          var mar = L.marker([event.latitud, event.longitud], {
+            icon: L.icon({
+              iconUrl: './assets/img/iconos/mapa/event.png',
+              iconSize: [20, 20],
+            })
+          }).bindPopup('<h5>Evento</h5><p>' + event.descripcion + '</p><a href="/events/' + event.id_evento + '">Ver Evento</a>');
+          marcas.addLayer(mar);
+        });
+      },
+      err => {
+        console.log(err);
+      });
+
+  
+    this.servicioUsuarios.getUsuarios().subscribe(
+      res => {
+        res.forEach(usu => {
+
+          if (usu.id != localStorage.getItem('id') && usu.last_latitud != 0 && usu.last_longitud != 0) {
+            const mar = L.marker([usu.last_latitud, usu.last_longitud], {
+              icon: L.icon({
+                iconUrl: './assets/img/iconos/mapa/user-pointer.png',
+                iconSize: [20, 20],
+              })
+            }).bindPopup('<h5>' + usu.nombre + ' ' + usu.apellidos + '</h5><br><a href="mailto:' + usu.email + '">' + usu.email + '</a>');
+            marcas.addLayer(mar);
+          }
+
+        });
+      },
+      err => {
+        console.log(err);
+      });
 
   }
+
 
 }
